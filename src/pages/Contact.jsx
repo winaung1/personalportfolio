e16df4613edf833ea24e474790556489c1e08648
Win Aung <91712429+winaung1@users.noreply.github.com>
2024-07-08 15:58:18 -0600

    done

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,7 +7,7 @@ export const Contact = () => {
         <p className='opacity-60 w-full md:w-1/2 mx-auto'>Contact me either way. I don't want to make a form contact. Just copy either the email or phone number. I'll get back to you.</p>
         <div className='bg-[#2b2d2d] flex flex-col items-center justify-center rounded h-80 my-4'>
             <h1 className='py-2 opacity-60'>Email</h1>
-            <p className='opacity-80 text-2xl'>winaung26@outlook.com</p>
+            <p className='opacity-80 text-2xl'>Winagencydesigns@gmail.com</p>
 
             <h1 className='py-2 opacity-60'>Phone</h1>
             <p className='opacity-80 text-2xl'>801-918-1587</p>
